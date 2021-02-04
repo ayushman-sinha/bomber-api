243fffbc39566abc3ad7956cdd3cde1dce9a4cfc
Nikita <belarussianhax@yandex.ru>
2021-02-05 00:24:26 +0300

    Создан main.js

---
+++ html/js/main.js
@@ -0,0 +1,30 @@
+$(document).on("click", "#submit", function(){
+    if($("input[name='number']").val() && $("input[name='loops']").val()){
+      $.ajax({
+        method: 'GET',
+        url: '/attack',
+        data:{
+          number: $("input[name='number']").val(),
+          loops: $("input[name='loops']").val()
+        },
+        dataType: 'json',
+        success: function(response) {
+          if(response.success == true){
+            Swal.fire({
+              icon: 'success',
+              title: 'OK',
+              text: response.text,
+              footer: '<a href="https://github.com/shketov/bomber-api" target="_blank">Мы на GitHub</a>'
+            })
+          }else{
+            Swal.fire({
+              icon: 'error',
+              title: 'Ошибка',
+              text: response.text,
+              footer: '<a href="https://github.com/shketov/bomber-api" target="_blank">Мы на GitHub</a>'
+            })
+          }
+        }
+      });
+    }
+  });
