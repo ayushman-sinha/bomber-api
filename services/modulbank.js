982ba4cbca8978f1d867d5422b721792283e4cfd
Nikita <belarussianhax@yandex.ru>
2021-02-05 15:58:15 +0300

    Правки

--- services/modulbank.js
+++ services/modulbank.js
@@ -4,7 +4,7 @@ module.exports.run = function(number){
 	if(number.toString().startsWith('7')){
 		request.post({
 			url:'https://my.modulbank.ru/api/v2/auth/phone', json: true,
-				form: {
+				body: {
 					phone:`${number.toString().slice(1)}`
 				}
 		}, 
