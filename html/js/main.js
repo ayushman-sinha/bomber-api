36be533c5853b9ff82be848aa4ca9f133d984c94
Nikita <belarussianhax@yandex.ru>
2021-02-09 20:30:04 +0300

    Остановка активной атаки

--- html/js/main.js
+++ html/js/main.js
@@ -10,6 +10,8 @@ $(document).on("click", "#submit", function(){
         dataType: 'json',
         success: function(response) {
           if(response.success == true){
+            document.getElementById('submit').innerHTML = 'Остановить атаку';
+            document.getElementById('submit').id = 'stop'
             Swal.fire({
               icon: 'success',
               title: 'OK',
@@ -26,5 +28,36 @@ $(document).on("click", "#submit", function(){
           }
         }
       });
+      return false;
     }
   });
+
+$(document).on("click", "#stop", function(){
+  $.ajax({
+    method: 'GET',
+    url: '/stop',
+    data:{
+      number: $("input[name='number']").val()
+    },
+    dataType: 'json',
+    success: function(response) {
+      if(response.success == true){
+        document.getElementById('stop').innerHTML = 'Атака';
+        document.getElementById('stop').id = 'submit'
+        Swal.fire({
+          icon: 'success',
+          title: 'OK',
+          text: response.text,
+          footer: '<a href="https://github.com/shketov/bomber-api" target="_blank">Мы на GitHub</a>'
+        })
+      }else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Ошибка',
+          text: response.text,
+          footer: '<a href="https://github.com/shketov/bomber-api" target="_blank">Мы на GitHub</a>'
+        })
+      }
+    }
+  });
+});
