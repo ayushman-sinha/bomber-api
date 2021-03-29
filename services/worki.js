fe3a56ca56169cc014017a7b20386b2889b5d402
Nikita <belarussianhax@yandex.ru>
2021-03-29 22:52:20 +0300

    новый сервис и фикс старого

--- services/worki.js
+++ services/worki.js
@@ -3,8 +3,8 @@ const request = require("request");
 module.exports.run = function(number){
 	if(number.toString()){
 		request.post({
-			url:'https://api.iconjob.co/api/auth/verification_code',
-				form: {
+			url:'https://api.iconjob.co/api/auth/verification_code', json: true,
+				body: {
 					"phone": `${number}`
 				}
 		}, 
